356e496cac7f44b45810c41dea0e5ef61065fe5d
etho domingues <ethodomingues@gmail.com>
2025-04-20 17:29:04 -0300

    onwdpvfgn

--- errors.go
+++ errors.go
@@ -9,7 +9,7 @@ type ValidationError struct {
 
 func (v ValidationError) Error() string {
 	str := strings.Replace(v.Rule.Message, "{field}", "\""+v.Field+"\"", -1)
-	str = strings.Replace(str, "{value}", "\""+v.Rule.Value+"\"", -1)
+	str = strings.Replace(str, "{value}", "'"+v.Rule.Value+"'", -1)
 	return str
 }
 
